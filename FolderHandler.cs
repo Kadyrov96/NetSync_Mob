6a133e4e8c7a017e30546b2d05afb90164fd1b61
Kadyrov96 <kadyrov96@mail.ru>
2017-02-06 23:29:15 +0300

    Class for operations with sync folder.

---
+++ FolderHandler.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace MobTest
+{
+    class FolderHandler
+    {
+        private string folderPath;
+        private IReadOnlyList<IStorageItem> folderElementsList;
+
+        public string FolderPath
+        {
+            private set
+            {
+                folderPath = value;
+            }
+            get
+            {
+                return folderPath;
+            }
+        }
+
+        private string[] folderElements;
+        public string[] FolderElements
+        {
+            private set
+            {
+                folderElements = value;
+            }
+            get
+            {
+                return folderElements;
+            }
+        }
+
+        internal async void SelectFolder()
+        {
+            var folderPicker = new FolderPicker();
+            folderPicker.SuggestedStartLocation = PickerLocationId.HomeGroup;
+            StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+            FolderPath = folder.Path;
+            if (folder != null)
+            {
+                MessageBox.Show("Picked folder: " + FolderPath, "Service information", MessageBoxButton.OK);
+                Windows.Storage.AccessCache.StorageApplicationPermissions.FutureAccessList.AddOrReplace("PickedFolderToken", folder);
+                folderElementsList = await folder.GetItemsAsync();
+            }
+            else
+            {
+                MessageBox.Show("Operation cancelled.", "Service information", MessageBoxButton.OK);
+            }
+        }
+
+        public bool IsFolderEmpty()
+        {
+            if (folderElements.Length == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void CreateServiceFile(string _full_file_path)
+        {
+            FileStream serviceFileCreator = File.Create(_full_file_path);
+            serviceFileCreator.Close();
+        }
+    }
+}
