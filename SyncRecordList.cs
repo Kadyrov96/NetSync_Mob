b9fe9636650593bb04741b5c5c8edf889c3bf5f3
Kadyrov96 <kadyrov96@mail.ru>
2017-02-08 22:20:39 +0300

    Helping class that handle with pairs of values: name and key of sync record.

---
+++ SyncRecordList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobTest
+{
+    class SyncRecordList
+    {
+        private int count;
+        public int Count
+        {
+            private set
+            {
+                count = names.Count;
+            }
+            get
+            {
+                return count;
+            }
+        }
+        public List<string> names;
+        public List<int> keys;
+        public SyncRecordList()
+        {
+            names = new List<string>();
+            keys = new List<int>();
+        }
+
+        public void AddRecord(string name, int key)
+        {
+            names.Add(name);
+            keys.Add(key);
+        }
+
+        public void RemoveAt(int index)
+        {
+            names.RemoveAt(index);
+            keys.RemoveAt(index);
+        }
+    }
+}
